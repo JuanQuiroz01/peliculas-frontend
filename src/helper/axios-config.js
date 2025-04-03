85a3cdd876e9775cf2ae75803eea9ec9a9e4af9c
Your Name <you@example.com>
2025-04-03 16:12:13 -0500

    render

--- src/helper/axios-config.js
+++ src/helper/axios-config.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:4000/'
+    baseURL: 'https://movie-api-backend-urao.onrender.com'
+    //baseURL: 'http://localhost:4000/'
 });
 
 export{
